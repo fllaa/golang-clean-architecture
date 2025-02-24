a06c1953e674b1a60da06ea8f8e1b01d52a094da
Fallah Andy Prakasa <me@flla.my.id>
2025-02-24 12:03:58 +0700

    chore: add config for log format

--- internal/config/logrus.go
+++ internal/config/logrus.go
@@ -7,9 +7,17 @@ import (
 
 func NewLogger(viper *viper.Viper) *logrus.Logger {
 	log := logrus.New()
+	format := viper.GetString("log.format")
 
 	log.SetLevel(logrus.Level(viper.GetInt32("log.level")))
-	log.SetFormatter(&logrus.JSONFormatter{})
+	switch format {
+	case "json":
+		log.SetFormatter(&logrus.JSONFormatter{})
+	case "text":
+		log.SetFormatter(&logrus.TextFormatter{
+			FullTimestamp: true,
+		})
+	}
 
 	return log
 }
